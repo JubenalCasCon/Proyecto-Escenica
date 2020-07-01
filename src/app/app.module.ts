f1e198328523919f5e13cdfb7261e2dfc36343b2
Jubenal <jubenalcastaneda@gmail.com>
2020-07-01 15:33:47 -0600

    Diseñados los apartados header y fotter, creados los componentes e implementadas las herramientas bootstrap y ng-bootrap como herramientas para el desarrollo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CarteleraComponent } from './cartelera/cartelera.component';
+import { CalendarioComponent } from './calendario/calendario.component';
+import { UbicacionComponent } from './ubicacion/ubicacion.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ElencoComponent } from './elenco/elenco.component';
 
+const routes:Routes=[{path:'',component:InicioComponent},
+                    {path:'cartelera',component:CarteleraComponent},  
+                    {path:'calendario',component:CalendarioComponent},
+                    {path:'ubicacion',component:UbicacionComponent},
+                    {path:'elenco',component:ElencoComponent}]
+                    
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    CarteleraComponent,
+    CalendarioComponent,
+    UbicacionComponent,
+    ElencoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
